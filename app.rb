c20381d01628ed17b61f89d8ea48b64e9865c77d
aadippel <aadippel@gmail.com>
2025-02-04 03:10:17 +0000

    added to currency variables to app.rb

--- app.rb
+++ app.rb
@@ -2,13 +2,24 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-erb(:flexible)
+
+  @from_currency =
+
+erb(:homepage)
 end
 
 get("/:currency_from") do
+
+  @from_currency =
+  @to_currency =
+
   erb(currency_exchange_start)
 end
 
 get("/:currency_from/:currency_to") do
+
+  @from_currency =
+  @to_currency =
+  
   erb(currency_exchange_result)
 end
