6da3d627d7fca7caa29bc75cb5725e5623172b93
aadippel <aadippel@gmail.com>
2025-02-04 16:46:48 +0000

    made app.rb and view template changes

--- app.rb
+++ app.rb
@@ -1,25 +1,67 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv"
+Dotenv.load
+
+require "http"
+
+require "json"
 
 get("/") do
 
-  @from_currency =
+# Assemble the API url, including the API key in the query string
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
 
+  # Use HTTP.get to retrieve the API data
+  @raw_response = HTTP.get(api_url)
+
+  # Get the body of the response as a string
+  @raw_string = @raw_response.to_s
+
+  # Convert the string to JSON
+  @parsed_data = JSON.parse(@raw_string)
+
+# Render a view template
 erb(:homepage)
 end
 
-get("/:currency_from") do
+get("/:from_currency") do
 
-  @from_currency =
-  @to_currency =
+  @original_currency = params.fetch("from_currency")
 
-  erb(currency_exchange_start)
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  
+  # Some more code to parse the URL and render a view template.
+  # (HINT: this code is identical to the first route, you just
+  # render a different view template at the end.)
+
+    # Use HTTP.get to retrieve the API data
+    @raw_response = HTTP.get(api_url)
+
+    # Get the body of the response as a string
+    @raw_string = @raw_response.to_s
+  
+    # Convert the string to JSON
+    @parsed_data = JSON.parse(@raw_string)
+
+  erb(:currency_exchange_start)
 end
 
-get("/:currency_from/:currency_to") do
+get("/:from_currency/:to_currency") do
 
-  @from_currency =
-  @to_currency =
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+    api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+
+        # Use HTTP.get to retrieve the API data
+        @raw_response = HTTP.get(api_url)
+
+        # Get the body of the response as a string
+        @raw_string = @raw_response.to_s
+      
+        # Convert the string to JSON
+        @exchange_rate = JSON.parse(@raw_string)
   
-  erb(currency_exchange_result)
+  erb(:currency_exchange_result)
 end
